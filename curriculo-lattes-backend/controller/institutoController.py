abadd46414e89ed0c06c150464b46f6c3618ff32
b1guuuu <costa.igor0108@gmail.com>
2024-03-11 22:17:36 -0300

    [backend] rota descritiva e limitados metódos HTTP

--- curriculo-lattes-backend/controller/institutoController.py
+++ curriculo-lattes-backend/controller/institutoController.py
@@ -7,18 +7,18 @@ app = Flask(__name__)
 
 
 # Função para chamar a lista
-@app.route('/listar') # utilize esse caminho para
+@app.route('/instituto/listar', methods=['GET']) # utilize esse caminho para
 def json_list():
     return json.dumps(BuscarInstituto())
 
 # Função para incerir os dados
-@app.route('/inserir/<nome>/<acronimo>') # utilize esse caminho para
+@app.route('/instituto/inserir/<nome>/<acronimo>', methods=['POST']) # utilize esse caminho para
 def inserir(nome, acronimo):
     CreateInstituto(nome, acronimo)
     return json.dumps("Inserido com sucesso")
 
 # Função para Deletar de acordo com o id
-@app.route('/deletar/<id>') # utilize esse caminho para
+@app.route('/instituto/deletar/<id>', methods=['DELETE']) # utilize esse caminho para
 def deletar(id):
     DeletarInstituto(id)
     return json.dumps("Deletado com sucesso")
