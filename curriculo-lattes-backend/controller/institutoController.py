f28354b0bf3598bfb84559eade86270bd92926d8
b1guuuu <costa.igor0108@gmail.com>
2024-03-13 17:03:22 -0300

    [backed] implementado filtro e ordenação

--- curriculo-lattes-backend/controller/institutoController.py
+++ curriculo-lattes-backend/controller/institutoController.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask
+from flask import request
 from flask_cors import CORS, cross_origin
 from model.institutoModel import *
 
@@ -27,5 +28,21 @@ def inserir(nome, acronimo):
 def deletar(id):
     DeletarInstituto(id)
     return json.dumps("Deletado com sucesso")
+
+@app.route('/instituto/atualizar/<id>/<nome>/<acronimo>', methods=['PUT']) # utilize esse caminho para
+@cross_origin()
+def atualizar(id, nome, acronimo):
+    UpdateInstituto(id, nome, acronimo)
+    return json.dumps("Atualizado com sucesso")
+
+
+@app.route('/instituto/filtrar/', methods=['GET']) # utilize esse caminho para
+@cross_origin()
+def filtrar():
+    nome = request.args.get('nome')
+    acronimo = request.args.get('acronimo')
+    orderBy = request.args.get('orderBy')
+    sort = request.args.get('sort')
+    return json.dumps(FilterInstituto(nome, acronimo, orderBy, sort))
 
 app.run()
--- curriculo-lattes-backend/model/institutoModel.py
+++ curriculo-lattes-backend/model/institutoModel.py
@@ -4,7 +4,7 @@ from config.configdb import conexao
 cursor = conexao.cursor()
 
 #CRUD
-#Create
+#CREATE
 def CreateInstituto(nome=None,acronimo=None):
     sql = 'INSERT INTO instituto (nome, acronimo) VALUES(%s, %s)'
     val = (nome,acronimo)
@@ -18,10 +18,31 @@ def BuscarInstituto():
     return resultado
 
 #UPDATE
+def UpdateInstituto(id=None,nome=None,acronimo=None):
+    sql = 'UPDATE instituto SET nome=%s, acronimo=%s WHERE id=%d'
+    val = (nome,acronimo,id)
+    cursor.execute(sql,val)
+    conexao.commit()
 
-#DELEAT
+#DELETE
 def DeletarInstituto(id):
     comando = ('DELETE FROM instituto WHERE id ='+str(id))
     cursor.execute(comando)
     conexao.commit()
 
+#FILTER
+def FilterInstituto(nome=None,acronimo=None,orderBy=None,sort=None):
+    sql = 'SELECT * FROM instituto '
+    if nome != 'null' and acronimo != 'null':
+        sql += "WHERE nome LIKE '%" + nome + "%' AND acronimo LIKE '%" + acronimo +"%' "
+    elif nome != 'null':
+        sql += "WHERE nome LIKE '%" + nome + "%' "
+    elif acronimo != 'null':
+        sql += "WHERE acronimo LIKE '%" + acronimo + "%' "
+
+    if orderBy != 'null':
+        sql += "ORDER BY " + orderBy + " " + sort
+        
+    cursor.execute(sql)
+    resultado = cursor.fetchall()
+    return resultado
