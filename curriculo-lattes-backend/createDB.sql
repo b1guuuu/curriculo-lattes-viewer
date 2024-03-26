dba4e003eab822737a1d12dfca50d498998a42d2
b1guuuu <costa.igor0108@gmail.com>
2024-03-26 16:22:11 -0300

    [backend] arquivo estrutura banco atualizado

--- curriculo-lattes-backend/createDB.sql
+++ curriculo-lattes-backend/createDB.sql
@@ -1,6 +1,15 @@
 CREATE TABLE IF NOT EXISTS instituto (
     id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL UNIQUE,
-    acronimo VARCHAR(255) NOT NULL,
+    acronimo VARCHAR(10) NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS pesquisador (
+    id INT NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE,
+    idInstituto INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (idInstituto) REFERENCES instituto(id)
 );
