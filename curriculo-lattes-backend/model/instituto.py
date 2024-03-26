e65ac05ba28dc4eca58903783a151087d7d98155
b1guuuu <costa.igor0108@gmail.com>
2024-03-26 11:25:04 -0300

    [backend] classes criadas

---
+++ curriculo-lattes-backend/model/instituto.py
@@ -0,0 +1,5 @@
+class Instituto:
+    def __init__(self, id, nome, acronimo):
+        self.id = id
+        self.nome = nome
+        self.acronimo = acronimo
---
+++ curriculo-lattes-backend/model/pesquisador.py
@@ -0,0 +1,6 @@
+class Instituto:
+    def __init__(self, id, nome, email, instituto):
+        self.id = id
+        self.nome = nome
+        self.email = email
+        self.instituto = instituto
