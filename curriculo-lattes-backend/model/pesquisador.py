1d8d99e2d015307c74a952cd1da97ce654b50553
b1guuuu <costa.igor0108@gmail.com>
2024-03-26 16:22:33 -0300

    [backend] ajustado model de pesquisador

--- curriculo-lattes-backend/model/pesquisador.py
+++ curriculo-lattes-backend/model/pesquisador.py
@@ -1,6 +1,6 @@
-class Instituto:
-    def __init__(self, id, nome, email, instituto):
+class Pesquisador:
+    def __init__(self, id, nome, email, idInstituto):
         self.id = id
         self.nome = nome
         self.email = email
-        self.instituto = instituto
+        self.idInstituto = idInstituto
