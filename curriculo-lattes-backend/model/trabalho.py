7ba2cddddcf7d2f3fbe5ee3bd6c1b09c57a8eee9
Guilherme Maia <xguilherme.ca.maiax@gmail.com>
2024-04-20 11:00:50 -0300

    Criação Model Trabalho

---
+++ curriculo-lattes-backend/model/trabalho.py
@@ -0,0 +1,7 @@
+class Trabalho:
+    def __init__(self, id, titulo, ano, local, tipo):
+        self.id = id
+        self.titulo = titulo
+        self.ano = ano
+        self.local = local
+        self.tipo = tipo
